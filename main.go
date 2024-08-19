b897b2b007e9726b617b574ef0a986580d98b477
박기현 <kihyun1998@penta.co.kr>
2024-08-19 20:06:04 +0900

    Feat: gRPC 엔드포인트 구현 (/start)

--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"time"
 
 	"github.com/segmentio/kafka-go"
 	lib "github.com/with-autro/autro-library"
+	pb "github.com/with-autro/autro-price/proto"
+	"google.golang.org/grpc"
 )
 
 const (
@@ -25,6 +30,12 @@ const (
 var kafkaBroker string
 var kafkaTopic string
 
+type server struct {
+	pb.UnimplementedPriceServiceServer
+	startOnce sync.Once
+	stopChan  chan struct{}
+}
+
 func init() {
 	kafkaBroker = os.Getenv("KAFKA_BROKER")
 	if kafkaBroker == "" {
@@ -36,6 +47,7 @@ func init() {
 	}
 }
 
+// candle data fetch 함수
 func fetchBTCCandleData(url string) ([]lib.CandleData, error) {
 
 	resp, err := http.Get(url)
@@ -71,6 +83,7 @@ func fetchBTCCandleData(url string) ([]lib.CandleData, error) {
 	return candles, nil
 }
 
+// kafka에 producer 함수
 func writeToKafka(writer *kafka.Writer, candles []lib.CandleData) error {
 	jsonData, err := json.Marshal(candles)
 	if err != nil {
@@ -84,6 +97,7 @@ func writeToKafka(writer *kafka.Writer, candles []lib.CandleData) error {
 	return err
 }
 
+// create kafka producer 함수
 func createWriter() *kafka.Writer {
 	return kafka.NewWriter(
 		kafka.WriterConfig{
@@ -93,6 +107,7 @@ func createWriter() *kafka.Writer {
 		})
 }
 
+// 시간 변환함수
 func utcToLocal(utcTime time.Time) time.Time {
 	loc, err := time.LoadLocation("Asia/Seoul")
 	if err != nil {
@@ -121,49 +136,90 @@ func getIntervalString(interval time.Duration) string {
 	}
 }
 
-func main() {
+func (s *server) Start(ctx context.Context, req *pb.StartRequest) (*pb.StartResponse, error) {
+	var err error
+	s.startOnce.Do(func() {
+		s.stopChan = make(chan struct{})
+		go s.runPriceCollection()
+		err = nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return &pb.StartResponse{Message: "Price collection started"}, nil
+}
+
+func (s *server) runPriceCollection() {
 	writer := createWriter()
 	defer writer.Close()
 
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
-
 	for {
-		now := time.Now()
-		nextFetch := nextIntervalStart(now, fetchInterval)
-		sleepDuration := nextFetch.Sub(now)
-
-		fmt.Printf("Waiting for %v untill next fetch at %v\n", sleepDuration.Round(time.Second), nextFetch.Format("2006-01-02 15:04:05"))
-
 		select {
-		case <-time.After(sleepDuration):
+		case <-s.stopChan:
+			log.Println("Stopping price collection")
+			return
+		default:
+			now := time.Now()
+			nextFetch := nextIntervalStart(now, fetchInterval)
+			sleepDuration := nextFetch.Sub(now)
+
+			log.Printf("Waiting for %v until next fetch at %v\n", sleepDuration.Round(time.Second), nextFetch.Format("2006-01-02 15:04:05"))
+
+			time.Sleep(sleepDuration)
+
 			url := fmt.Sprintf("%s?symbol=BTCUSDT&interval=%s&limit=%d", binanceKlineAPI, getIntervalString(sleepDuration), candleLimit)
 
 			candles, err := fetchBTCCandleData(url)
 			if err != nil {
-				fmt.Printf("Error fetching candle data: %v\n", err)
+				log.Printf("Error fetching candle data: %v\n", err)
 				continue
 			}
 
 			err = writeToKafka(writer, candles)
 			if err != nil {
-				fmt.Printf("Error producing to Kafka: %v\n", err)
+				log.Printf("Error producing to Kafka: %v\n", err)
 			} else {
-				fmt.Printf("Successfully sent %d candle data to Kafka\n", len(candles))
+				log.Printf("Successfully sent %d candle data to Kafka\n", len(candles))
 				if len(candles) > 0 {
 					firstCandle := candles[0]
 					lastCandle := candles[len(candles)-1]
 					firstTime := utcToLocal(time.Unix(firstCandle.OpenTime/1000, 0))
 					lastTime := utcToLocal(time.Unix(lastCandle.CloseTime/1000, 0))
-					fmt.Printf("Data range (Local Time): %v to %v\n",
+					log.Printf("Data range (Local Time): %v to %v\n",
 						firstTime.Format("2006-01-02 15:04:05"),
 						lastTime.Format("2006-01-02 15:04:05"))
 				}
 			}
-
-		case <-signals:
-			fmt.Println("Interrupt received, shutting down...")
-			return
 		}
 	}
+}
+
+func main() {
+	port := os.Getenv("GRPC_PORT")
+	if port == "" {
+		port = "50051"
+	}
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
+	if err != nil {
+		log.Fatalf("Failed to listen : %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterPriceServiceServer(s, &server{})
+
+	go func() {
+		log.Printf("autro-price service listening on : %s", port)
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	<-sigChan
+
+	log.Println("Shutting down gracefully")
+	s.GracefulStop()
 }
