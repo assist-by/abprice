ec573020a1a884bd1350e5fbd4884e94c7807334
pentakihyun <71832733+kihyun1998@users.noreply.github.com>
2024-08-31 15:32:12 +0900

    Feat: start API 엔드 포인트 구현

--- main.go
+++ main.go
@@ -9,8 +9,10 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/segmentio/kafka-go"
 	lib "github.com/with-autro/autro-library"
 )
@@ -29,6 +31,8 @@ var (
 	host              string
 	port              string
 	registrationTopic string
+	isRunning         bool
+	runningMutex      sync.Mutex
 )
 
 func init() {
@@ -175,30 +179,22 @@ func createRegistrationWriter() *kafka.Writer {
 		})
 }
 
-func main() {
-	writer := createWriter()
-	defer writer.Close()
-
-	registrationWriter := createRegistrationWriter()
-	defer registrationWriter.Close()
-
-	if err := registerService(registrationWriter); err != nil {
-		log.Printf("Failed to register service: %v\n", err)
-	}
-
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
-
+func startService(ctx context.Context, writer *kafka.Writer) {
 	for {
-		now := time.Now()
-		nextFetch := nextIntervalStart(now, fetchInterval)
-		sleepDuration := nextFetch.Sub(now)
-
-		log.Printf("Waiting for %v untill next fetch at %v\n", sleepDuration.Round(time.Second), nextFetch.Format("2006-01-02 15:04:05"))
-
 		select {
-		case <-time.After(sleepDuration):
-			url := fmt.Sprintf("%s?symbol=BTCUSDT&interval=%s&limit=%d", binanceKlineAPI, getIntervalString(sleepDuration), candleLimit)
+		case <-ctx.Done():
+			log.Println("Stopping price collection...")
+			return
+		default:
+			now := time.Now()
+			nextFetch := nextIntervalStart(now, fetchInterval)
+			sleepDuration := nextFetch.Sub(now)
+
+			log.Printf("Waiting for %v until next fetch at %v\n", sleepDuration.Round(time.Second), nextFetch.Format("2006-01-02 15:04:05"))
+
+			time.Sleep(sleepDuration)
+
+			url := fmt.Sprintf("%s?symbol=BTCUSDT&interval=%s&limit=%d", binanceKlineAPI, getIntervalString(fetchInterval), candleLimit)
 
 			candles, err := fetchBTCCandleData(url)
 			if err != nil {
@@ -221,10 +217,93 @@ func main() {
 						lastTime.Format("2006-01-02 15:04:05"))
 				}
 			}
-
-		case <-signals:
-			log.Println("Interrupt received, shutting down...")
-			return
 		}
 	}
+}
+
+// POST
+// start service를 /start API로 받아서 실행하는 함수
+func startHandler(c *gin.Context) {
+	runningMutex.Lock()
+	defer runningMutex.Unlock()
+
+	if isRunning {
+		c.JSON(http.StatusOK, gin.H{"message": "autro-price is already running"})
+		return
+	}
+
+	isRunning = true
+
+	go startService(c.Request.Context(), createWriter())
+	c.JSON(http.StatusOK, gin.H{"message": "autro-price started successfully"})
+}
+
+// POST
+// stop service를 /stop API로 받아서 실행하는 함수
+// stop service 아직 미구현
+func stopHandler(c *gin.Context) {
+	runningMutex.Lock()
+	defer runningMutex.Unlock()
+
+	if !isRunning {
+		c.JSON(http.StatusOK, gin.H{"message": "autro-price is not running"})
+	}
+
+	isRunning = false
+	c.JSON(http.StatusOK, gin.H{"message": "autro-price stopped successfully"})
+}
+
+// GET
+// status를 /status API로 받아서 실행하는 함수
+func statusHandler(c *gin.Context) {
+	runningMutex.Lock()
+	defer runningMutex.Unlock()
+
+	status := "stopped"
+	if isRunning {
+		status = "running"
+	}
+	c.JSON(http.StatusOK, gin.H{"status": status})
+}
+
+func main() {
+	writer := createWriter()
+	defer writer.Close()
+
+	registrationWriter := createRegistrationWriter()
+	defer registrationWriter.Close()
+
+	if err := registerService(registrationWriter); err != nil {
+		log.Printf("Failed to register service: %v\n", err)
+	}
+
+	router := gin.Default()
+	router.POST("/start", startHandler)
+	router.POST("/stop", stopHandler)
+	router.GET("/status", statusHandler)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to start server : $v", err)
+		}
+	}()
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+	<-signals
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
+
 }
